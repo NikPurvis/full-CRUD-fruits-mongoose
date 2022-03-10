3572f0f2a40a7eacf74723e69913672603642f79
NikPurvis <nikpurvis@gmail.com>
2022-03-10 10:43:51 -0700

    Add signup page and functionality

--- controllers/user.js
+++ controllers/user.js
@@ -2,9 +2,9 @@
 // Import dependencies
 ///////////////////////////////////////
 // This allows us to load our env variables
-const express = require('express')
-const User = require('../models/user')
-const bcrypt = require('bcryptjs')
+const express = require("express")
+const User = require("../models/user")
+const bcrypt = require("bcryptjs")
 
 ///////////////////////////////////////
 // Create router
@@ -17,43 +17,40 @@ const router = express.Router()
 
 // Two sign-up routes:
 // 1. GET to render the sign-up form
-router.get('/signup', (req, res) => {
-    res.send('sign up page')
+router.get("/signup", (req, res) => {
+    res.render("users/signup")
 })
 // 2. POST to send the signup info
-router.post('/signup', (req, res) => {
-    res.send('signup -> post')
+router.post("/signup", async (req, res) => {
+    // console.log("Initial req.body in signup:", req.body)
+    // 1. Encrypt our password
+    req.body.password = await bcrypt.hash(
+        req.body.password, 
+        await bcrypt.genSalt(10)
+    )
+    // console.log("req.body after hash:", req.body)
+    // 2. Create a new user
+    User.create(req.body)
+        // 3. If created successfully redirect to login
+        .then(user => {
+            res.redirect("/user/login")
+        })
+        // 4. If an error occurs, send error
+        .catch(error => {
+            console.log(error)
+            res.json(error)
+        })
 })
-    // res.send("signup -> POST")
-    // console.log("This is initial req.body in signup:", req.body)
-    // first encrypt our password
-    // req.body.password = await bcrypt.hash(
-    //     req.body.password,
-    //     await bcrypt.genSalt(10)        
-    //     )
-    // console.log("req.body after password:", req.body)
-    // create a new user
-//     User.create(req.body)
-//         // if created successfully, redirect to login
-//         .then(user => {
-//             res.redirect("/user/login")
-//         })
-//         // if error occurs, send the error
-//         .catch(error => {
-//             console.log(error)
-//             res.json(error))
-// })
 
 // Two login routes:
 // 1. GET to render the login form
-
-router.get('/login', (req, res) => {
-    res.send('login page')
+router.get("/login", (req, res) => {
+    res.send("login page")
 })
 
 // 2. POST to send the login info (and create a session)
-router.post('/login', (req, res) => {
-    res.send('login -> post')
+router.post("/login", (req, res) => {
+    res.send("login -> post")
 })
 
 // Signout route -> destroy the session
